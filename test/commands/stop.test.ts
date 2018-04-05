f45159bf801b134ee9b933d8fa1f37d02b63a2b3
David Jones <dgjones@gmail.com>
2018-04-05 10:40:10 -0700

    Fix test inconsistencies. Ensure the agent is stopped or started before running a test

--- test/commands/stop.test.ts
+++ test/commands/stop.test.ts
@@ -1,34 +1,29 @@
 import {expect, test} from '@oclif/test'
-import Start from '../../src/commands/start'
-import Stop from '../../src/commands/stop'
 
 describe('stop', () => {
-  Start.run([])
-
   test
   .stdout()
+  .command(['start'])
   .command(['stop'])
   .it('runs stop', ctx => {
     expect(ctx.stdout).to.contain('gracefully stopping percy-agent')
     expect(ctx.stdout).to.contain('percy-agent has stopped.')
   })
 
-  Start.run([]).then(() => {
-    test
-      .stdout()
-      .command(['stop', '--force'])
-      .it('runs stop --force', ctx => {
-        expect(ctx.stdout).to.contain('forcefully stopping percy-agent')
-        expect(ctx.stdout).to.contain('percy-agent has stopped.')
-      })
+  test
+  .stdout()
+  .command(['start'])
+  .command(['stop', '--force'])
+  .it('runs stop', ctx => {
+    expect(ctx.stdout).to.contain('forcefully stopping percy-agent')
+    expect(ctx.stdout).to.contain('percy-agent has stopped.')
   })
-
-  Stop.run([])
 
   test
   .stdout()
   .command(['stop'])
+  .command(['stop', '--force'])
   .it('runs stop', ctx => {
-    expect(ctx.stdout).to.contain('percy-agent is already stopped.')
+    expect(ctx.stdout).to.contain('percy-agent is already stopped')
   })
 })
