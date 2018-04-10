2a66b339801ad7d10ad58733c6a7e9fc8be31983
David Jones <dgjones@gmail.com>
2018-04-10 15:12:54 -0700

    include generated percy-agent.js file

--- src/public/percy-agent.d.ts
+++ src/public/percy-agent.d.ts
@@ -1,11 +1,29 @@
-declare module "percy" {
-    export default interface SnapshotOptions {
-        enable_javascript?: boolean;
-        widths?: [number];
-    }
-    export default class Percy {
-        clientUserAgent: string | null;
-        constructor(clientUserAgent?: string);
-        snapshot(name: string, options: SnapshotOptions): void;
-    }
+interface Request {
+    /**
+     * URL of the request.
+     * https://developer.mozilla.org/en-US/docs/Web/API/PerformanceEntry/name
+     */
+    name: string;
+}
+declare class RequestManifest {
+    /**
+     * Capture a list of URLs for resources requested by this page.
+     */
+    capture(): string[];
+}
+interface SnapshotOptions {
+    enableJavascript?: boolean;
+    widths?: number[];
+    minimumHeight?: number;
+}
+declare class Percy {
+    clientUserAgent: string | null;
+    beforeSnapshot: any;
+    readonly defaultDoctype: string;
+    constructor(clientUserAgent?: string, beforeSnapshot?: any);
+    snapshot(name: string, options: SnapshotOptions): void;
+    private domSnapshot();
+    private getDoctype();
+    private doctypeToString(doctype);
+    private stabalizePage();
 }
--- src/public/percy-agent.js
+++ src/public/percy-agent.js
@@ -1,14 +1,56 @@
-define("percy", ["require", "exports"], function (require, exports) {
-    "use strict";
-    exports.__esModule = true;
-    var Percy = /** @class */ (function () {
-        function Percy(clientUserAgent) {
-            this.clientUserAgent = clientUserAgent || null;
+"use strict";
+var RequestManifest = /** @class */ (function () {
+    function RequestManifest() {
+    }
+    /**
+     * Capture a list of URLs for resources requested by this page.
+     */
+    RequestManifest.prototype.capture = function () {
+        var requests = performance.getEntriesByType('resource');
+        return requests.map(function (request) { return request.name; });
+    };
+    return RequestManifest;
+}());
+var Percy = /** @class */ (function () {
+    function Percy(clientUserAgent, beforeSnapshot) {
+        this.defaultDoctype = '<!DOCTYPE html>';
+        this.clientUserAgent = clientUserAgent || null;
+        this.beforeSnapshot = beforeSnapshot || null;
+    }
+    Percy.prototype.snapshot = function (name, options) {
+        if (this.beforeSnapshot) {
+            this.beforeSnapshot();
         }
-        Percy.prototype.snapshot = function (name, options) {
-            console.log("taking snapshot " + name + ". js: " + options.enable_javascript + ". widths: " + options.widths);
-        };
-        return Percy;
-    }());
-    exports["default"] = Percy;
-});
+        this.stabalizePage();
+        var requestManifest = new RequestManifest();
+        console.log('TAKING SNAPSHOT\n' +
+            ("name: " + name + "\n") +
+            ("enableJavascript: " + options.enableJavascript + ".\n") +
+            ("widths: " + options.widths + ".\n") +
+            ("clientUserAgent: " + this.clientUserAgent + ".\n") +
+            ("requestManifest: " + requestManifest.capture() + "\n") +
+            ("domSnapshot: " + this.domSnapshot()));
+    };
+    Percy.prototype.domSnapshot = function () {
+        var doctype = this.getDoctype();
+        var dom = document.documentElement.outerHTML;
+        return doctype + dom;
+    };
+    Percy.prototype.getDoctype = function () {
+        return document.doctype.name ? this.doctypeToString(document.doctype) : this.defaultDoctype;
+    };
+    Percy.prototype.doctypeToString = function (doctype) {
+        var publicDeclaration = doctype.publicId ? " PUBLIC \"" + doctype.publicId + "\" " : '';
+        var systemDeclaration = doctype.systemId ? " SYSTEM \"" + doctype.systemId + "\" " : '';
+        return "<!DOCTYPE " + doctype.name + publicDeclaration + systemDeclaration + '>';
+    };
+    Percy.prototype.stabalizePage = function () {
+        // Apply various hacks to the pages
+        // freeze jQuery etc.
+    };
+    return Percy;
+}());
+// Run this on test page:
+// var beforeSnapshot = function() { console.log('beforeSnapshot'); };
+// var percy = new Percy('dave agent');
+// percy.snapshot('my snapshot', { enable_javascript: false, widths: [300, 500] });
