45f65ba42b06a824ee9af87309e96a9332312cf9
David Jones <dgjones@gmail.com>
2018-04-16 16:44:38 -0700

    added mocha/chai tests for stop command

--- test/commands/stop.test.ts
+++ test/commands/stop.test.ts
@@ -1,20 +1,44 @@
-// const {stdout} = require('stdout-stderr')
-// import {describe} from 'mocha'
-// import Start from '../../src/commands/start'
-// import Stop from '../../src/commands/stop'
-// import * as chai from 'chai'
-// const expect = chai.expect
+import {describe} from 'mocha'
+import Start from '../../src/commands/start'
+import Stop from '../../src/commands/stop'
+import * as chai from 'chai'
+import {captureStdOut} from '../helpers/stdout'
+const expect = chai.expect
 
-// describe('Stop', () => {
-//   let subject = Stop
+describe('Start', () => {
+  beforeEach(async () => {
+    await captureStdOut(async () => Start.run([]))
+  })
 
-//   describe('#run', async () => {
-//     await Start.run([])
+  describe('#stop', () => {
+    it('stops percy agent gracefully', async () => {
+      let stdout = await captureStdOut(async () => {
+        await Stop.run([])
+      })
 
-//     stdout.start()
-//     await subject.run([])
-//     stdout.stop()
+      expect(stdout).to.match(/\[info\] gracefully stopping percy-agent\[\d+\]/)
+      expect(stdout).to.match(/\[info\] percy-agent\[\d+\] has stopped/)
+    })
 
-//     expect(stdout.output).to.match(/foo/)
-//   })
-// })
+    it('stops percy agent forcefully', async () => {
+      let stdout = await captureStdOut(async () => {
+        await Stop.run(['--force'])
+      })
+
+      expect(stdout).to.match(/\[info\] forcefully stopping percy-agent\[\d+\]/)
+      expect(stdout).to.match(/\[info\] percy-agent\[\d+\] has stopped/)
+    })
+
+    it('warns you when percy agent is already stopped', async () => {
+      await captureStdOut(async () => {
+        await Stop.run([])
+      })
+
+      let stdout = await captureStdOut(async () => {
+        await Stop.run([])
+      })
+
+      expect(stdout).to.match(/\[info\] percy-agent is already stopped/)
+    })
+  })
+})
