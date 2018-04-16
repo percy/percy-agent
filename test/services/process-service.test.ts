a7d5c548cfef09a8c3d69a8210209fc1c9a7ff60
David Jones <dgjones@gmail.com>
2018-04-16 16:52:38 -0700

    add back in ProcessService tests

--- test/services/process-service.test.ts
+++ test/services/process-service.test.ts
@@ -1,52 +1,52 @@
-// import {describe} from 'mocha'
-// import {expect} from 'chai'
-// import ProcessService from '../../src/services/process-service'
-// import {createPidFile, deletePidFile} from '../helpers/process'
+import {describe} from 'mocha'
+import {expect} from 'chai'
+import ProcessService from '../../src/services/process-service'
+import {createPidFile, deletePidFile} from '../helpers/process'
 
-// describe('ProcessService', () => {
-//   let subject = new ProcessService()
+describe('ProcessService', () => {
+  let subject = new ProcessService()
 
-//   describe('#isRunning', () => {
-//     it('returns false if not running', async () => {
-//       await deletePidFile()
-//       expect(await subject.isRunning()).to.equal(false)
-//     })
+  describe('#isRunning', () => {
+    it('returns false if not running', async () => {
+      await deletePidFile()
+      expect(await subject.isRunning()).to.equal(false)
+    })
 
-//     it('returns true if running', async () => {
-//       await createPidFile()
-//       expect(await subject.isRunning()).to.equal(true)
-//       await deletePidFile()
-//     })
-//   })
+    it('returns true if running', async () => {
+      await createPidFile()
+      expect(await subject.isRunning()).to.equal(true)
+      await deletePidFile()
+    })
+  })
 
-//   describe('#getPid', () => {
-//     it('returns the pid', async () => {
-//       await createPidFile(123)
-//       expect(await subject.getPid()).to.equal(123)
-//       await deletePidFile()
-//     })
-//   })
+  describe('#getPid', () => {
+    it('returns the pid', async () => {
+      await createPidFile(123)
+      expect(await subject.getPid()).to.equal(123)
+      await deletePidFile()
+    })
+  })
 
-//   describe('#runDetached', () => {
-//     it('returns pid of detached process', async () => {
-//       expect(await subject.runDetached(['bin/run'])).to.be.a('number')
-//       await deletePidFile()
-//     })
+  describe('#runDetached', () => {
+    it('returns pid of detached process', async () => {
+      expect(await subject.runDetached(['bin/run'])).to.be.a('number')
+      await deletePidFile()
+    })
 
-//     it('returns null is process is already running', async () => {
-//       await createPidFile()
-//       expect(await subject.runDetached(['bin/run'])).to.equal(null)
-//       await deletePidFile()
-//     })
-//   })
+    it('returns null is process is already running', async () => {
+      await createPidFile()
+      expect(await subject.runDetached(['bin/run'])).to.equal(null)
+      await deletePidFile()
+    })
+  })
 
-//   describe('#kill', () => {
-//     it('kills a running process', async () => {
-//       await subject.runDetached(['bin/run', 'start', 'attached'])
-//       expect(await subject.isRunning()).to.equal(true)
+  describe('#kill', () => {
+    it('kills a running process', async () => {
+      await subject.runDetached(['bin/run', 'start', 'attached'])
+      expect(await subject.isRunning()).to.equal(true)
 
-//       await subject.kill()
-//       expect(await subject.isRunning()).to.equal(false)
-//     })
-//   })
-// })
+      await subject.kill()
+      expect(await subject.isRunning()).to.equal(false)
+    })
+  })
+})
