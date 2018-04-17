6f39b3fe02f39497eb6925a337e9dc96b5dd5933
David Jones <dgjones@gmail.com>
2018-04-16 17:48:43 -0700

    use the replay package to record http requests

--- test/helpers/init.js
+++ test/helpers/init.js
@@ -1,2 +1,5 @@
-const path = require('path')
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
+const path = require('path');
+process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
+
+const Replay = require('replay');
+Replay.fixtures = './test/fixtures';
