5cc53d22304f233f7f29d9c1b9d9f42e1f3b9566
David Jones <dgjones@gmail.com>
2018-04-19 15:26:15 -0700

    add test coverage for SnapshotService#createSnapshot

--- test/services/snapshot-service.test.ts
+++ test/services/snapshot-service.test.ts
@@ -1,13 +1,24 @@
-// import {describe} from 'mocha'
-// import {expect} from 'chai'
-// import SnapshotService from '../../src/services/snapshot-service'
+import {describe} from 'mocha'
+import {expect} from 'chai'
+import SnapshotService from '../../src/services/snapshot-service'
+import * as nock from 'nock'
 
-// describe('SnapshotService', () => {
-//   let subject = new SnapshotService()
+describe('SnapshotService', () => {
+  let subject = new SnapshotService()
 
-//   describe('#createSnapshot', () => {
-//     it('creates a snapshot', async () => {
-//       expect(await subject.createSnapshot('test')).to.equal(true)
-//     })
-//   })
-// })
+  beforeEach(async () => {
+    nock('https://percy.io')
+      .post(/\/api\/v1\/builds\/\d+\/snapshots/)
+      .reply(200, {data: {id: 1}})
+  })
+
+  describe('#createSnapshot', () => {
+    it('creates a snapshot', async () => {
+      let snapshotId = await subject.createSnapshot(
+        1, 'my test', 'http://localhost/index.html', '<html><body></body></html>'
+      )
+
+      expect(snapshotId).to.equal(1)
+    })
+  })
+})
