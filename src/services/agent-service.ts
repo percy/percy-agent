cee17baab0d5039c8eee4ead56e9e3f5c0197b07
David Jones <dgjones@gmail.com>
2018-05-04 15:06:00 -0700

    refactor BuildService to hang around and maintain state about the current build

--- src/services/agent-service.ts
+++ src/services/agent-service.ts
@@ -10,7 +10,8 @@ import ProcessService from './process-service'
 export default class AgentService {
   readonly app: express.Application
   server: Server | null = null
-  buildId: number | null = null
+  snapshotService: SnapshotService | null = null
+  buildService: BuildService
 
   constructor() {
     this.app = express()
@@ -24,15 +25,19 @@ export default class AgentService {
     this.app.post('/percy/stop', this.handleStop.bind(this))
 
     this.app.get('/percy/healthcheck', this.handleHealthCheck.bind(this))
+
+    this.buildService = new BuildService()
   }
 
   async start(port: number) {
     this.server = this.app.listen(port)
-    this.buildId = await new BuildService().createBuild()
+
+    let buildId = await this.buildService.createBuild()
+    this.snapshotService = new SnapshotService(buildId)
   }
 
   async stop() {
-    if (this.buildId) { await new BuildService().finalizeBuild(this.buildId) }
+    await this.buildService.finalizeBuild()
     if (this.server) { await this.server.close() }
   }
 
@@ -40,12 +45,8 @@ export default class AgentService {
     // Use this once we have snapshot user agent support
     // let userAgent = request.headers['user-agent']
 
-    const snapshotService = new SnapshotService()
-    let snapshotId = null
-
-    if (this.buildId) {
-      snapshotId = await snapshotService.createSnapshot(
-        this.buildId,
+    if (this.snapshotService) {
+      await this.snapshotService.createSnapshot(
         request.body.name,
         request.body.url,
         request.body.domSnapshot,
@@ -54,11 +55,9 @@ export default class AgentService {
         request.body.widths
       )
 
-      if (snapshotId) {
-        await snapshotService.finalizeSnapshot(snapshotId)
-        logger.info(`snapshot taken: '${request.body.name}'`)
-        return response.json({success: true})
-      }
+      logger.info(`snapshot taken: '${request.body.name}'`)
+
+      return response.json({success: true})
     }
 
     return response.json({success: false})
