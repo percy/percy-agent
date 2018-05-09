c89833ccdedb1bca380cddd9a9c66fe7a99e4cb6
David Jones <dgjones@gmail.com>
2018-05-09 12:00:46 -0700

    test coverage for RequestService#createLocalCopies

--- test/services/request-service.test.ts
+++ test/services/request-service.test.ts
@@ -53,4 +53,31 @@ describe('RequestService', () => {
       ])
     })
   })
+
+  describe('#createLocalCopies', () => {
+    beforeEach(async () => {
+      nock('https://percy.io')
+        .get('/logo.svg')
+        .reply(200, '<svg></svg>', {'Content-Type': 'image/svg+xml'})
+
+      nock('https://percy.io')
+        .get('/app.css')
+        .reply(200, 'body { background: green; }', {'Content-Type': 'image/svg+xml'})
+    })
+
+    it('creates local copies', async () => {
+      let requestManifest: any[] = [
+        'https://percy.io/app.css',
+        'https://percy.io/logo.svg',
+      ]
+      let localCopies = await subject.createLocalCopies(requestManifest)
+
+      let expectedResult = new Map(
+        [['https://percy.io/app.css', './tmp/39c6ed7372d209cb3d8b85797161b7cadc7fa0c76370479dbe543f6c11c30b06'],
+         ['https://percy.io/logo.svg', './tmp/b12e0d83ce2357d80b89c57694814d0a3abdaf8c40724f2049af8b7f01b7812b']]
+      )
+
+      expect(localCopies).to.deep.equal(expectedResult)
+    })
+  })
 })
