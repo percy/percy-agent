d269afc2876f150e300538558a58f06927847488
David Jones <dgjones@gmail.com>
2018-08-30 11:04:01 -0700

    capitalize these constants

--- src/services/process-service.ts
+++ src/services/process-service.ts
@@ -2,13 +2,13 @@ import * as childProcess from 'child_process'
 import * as fs from 'fs'
 
 export default class ProcessService {
-  static pidPath = './.percy-agent.pid'
-  static logPath = './percy-agent-process.log'
+  static PID_PATH = './.percy-agent.pid'
+  static LOG_PATH = './percy-agent-process.log'
 
   async runDetached(args: string[]): Promise<number | null> {
     if (await this.isRunning()) { return null }
 
-    const logFile = fs.openSync(ProcessService.logPath, 'a+')
+    const logFile = fs.openSync(ProcessService.LOG_PATH, 'a+')
 
     const spawnedProcess = childProcess.spawn(process.argv[0], args, {
       detached: false,
@@ -23,11 +23,11 @@ export default class ProcessService {
   }
 
   async isRunning(): Promise<boolean> {
-    return fs.existsSync(ProcessService.pidPath)
+    return fs.existsSync(ProcessService.PID_PATH)
   }
 
   async getPid(): Promise<number> {
-    let pidFileContents: Buffer = await fs.readFileSync(ProcessService.pidPath)
+    let pidFileContents: Buffer = await fs.readFileSync(ProcessService.PID_PATH)
     return parseInt(pidFileContents.toString('utf8').trim())
   }
 
@@ -37,12 +37,12 @@ export default class ProcessService {
     if (running) {
       const pid = await this.getPid()
 
-      await fs.unlinkSync(ProcessService.pidPath)
+      await fs.unlinkSync(ProcessService.PID_PATH)
       process.kill(pid, 'SIGHUP')
     }
   }
 
   private async writePidFile(pid: number) {
-    await fs.writeFileSync(ProcessService.pidPath, pid)
+    await fs.writeFileSync(ProcessService.PID_PATH, pid)
   }
 }
--- test/helpers/process.ts
+++ test/helpers/process.ts
@@ -2,13 +2,13 @@ import * as fs from 'fs'
 import ProcessService from '../../src/services/process-service'
 
 export async function createPidFile(pid: number = 123) {
-  fs.writeFileSync(ProcessService.pidPath, pid)
+  fs.writeFileSync(ProcessService.PID_PATH, pid)
 }
 
 export async function deletePidFile() {
-  if (await pidFileExists()) { fs.unlinkSync(ProcessService.pidPath) }
+  if (await pidFileExists()) { fs.unlinkSync(ProcessService.PID_PATH) }
 }
 
 export async function pidFileExists(): Promise<boolean> {
-  return fs.existsSync(ProcessService.pidPath)
+  return fs.existsSync(ProcessService.PID_PATH)
 }
