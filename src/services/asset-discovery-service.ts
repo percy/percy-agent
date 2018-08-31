13f95ba2d33ce8d3eb84261f95590ec8d5a30d1c
David Jones <dgjones@gmail.com>
2018-08-31 14:22:00 -0700

    extract waitForNetworkIdle into a file since it's a nicely separated function

--- src/services/asset-discovery-service.ts
+++ src/services/asset-discovery-service.ts
@@ -3,6 +3,7 @@ import ResponseService from './response-service'
 import logger, {logError} from '../utils/logger'
 import * as puppeteer from 'puppeteer'
 import unique from '../utils/unique-array'
+import waitForNetworkIdle from '../utils/wait-for-network-idle'
 
 export default class AssetDiscoveryService extends PercyClientService {
   readonly NETWORK_IDLE_TIMEOUT = 25 // ms
@@ -87,37 +88,4 @@ export default class AssetDiscoveryService extends PercyClientService {
     this.browser = null
   }
 
-}
-
-function waitForNetworkIdle(page: puppeteer.Page, timeout = 50, maxInflightRequests = 0) {
-  page.on('request', onRequestStarted)
-  page.on('requestfinished', onRequestFinished)
-  page.on('requestfailed', onRequestFinished)
-
-  let inflight = 0
-  let fulfill: any
-  let promise = new Promise(x => fulfill = x)
-  let timeoutId = setTimeout(onTimeoutDone, timeout)
-  return promise
-
-  function onTimeoutDone() {
-    page.removeListener('request', onRequestStarted)
-    page.removeListener('requestfinished', onRequestFinished)
-    page.removeListener('requestfailed', onRequestFinished)
-    fulfill()
-  }
-
-  function onRequestStarted() {
-    ++inflight
-    if (inflight > maxInflightRequests)
-      clearTimeout(timeoutId)
-  }
-
-  function onRequestFinished() {
-    if (inflight === 0)
-      return
-    --inflight
-    if (inflight === maxInflightRequests)
-      timeoutId = setTimeout(onTimeoutDone, timeout)
-  }
 }
