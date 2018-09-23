e6d5466efc785503e9bf84b34e0e1f8d3ed679d9
David Jones <dgjones@gmail.com>
2018-09-22 20:44:14 -0700

    output [percy] at the start of each log line in magenta

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,20 @@
 import * as winston from 'winston'
+import * as colors from 'colors'
 
 let logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       level: (process.env.LOG_LEVEL || 'info'),
-      colorize: true,
+      showLevel: false,
+      label: colors.magenta('percy'),
     })
   ]
 })
 
-export function profile(id: string, meta?: any, callback?: (err: Error, level: string, msg: string, meta: any) => void): winston.LoggerInstance | undefined {
+export function profile(
+  id: string, meta?: any,
+  callback?: (err: Error, level: string, msg: string, meta: any) => void
+): winston.LoggerInstance | undefined {
   if (process.env.LOG_LEVEL === 'debug') {
     return logger.profile(id, id, meta, callback)
   }
