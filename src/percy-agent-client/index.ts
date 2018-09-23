aef28b271e8fa05f5ac4335eeda014ad0510356d
David Jones <dgjones@gmail.com>
2018-09-22 20:46:57 -0700

    add comment clarifying the reason for the default export

--- src/percy-agent-client/index.ts
+++ src/percy-agent-client/index.ts
@@ -1,1 +1,3 @@
+// This is setup like this so you can include percy-agent.js onto your webpage
+// and then simply use `var percyAgent = new PercyAgent()` to create a new client instance.
 module.exports = require('./percy-agent').default
