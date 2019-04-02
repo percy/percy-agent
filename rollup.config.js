757ebcce10daa952befcb307ee2e984be05aaee6
Robert DeLuca <robertdeluca19@gmail.com>
2019-04-02 09:16:08 -0500

    Introduce rollup config
    This is to resolve the error where winston was being being included in
    various webpack bundles. We change the `main` entry point of the
    `@percy/agent` package so only the file that's ready for a browser is
    bundled. Then we transpile that file fully with babel and rollup so
    their webpack config isn't looking to compile it too.
    
    This solves https://github.com/percy/percy-cypress/issues/58 fully.

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+
+export default {
+  input: "src/percy-agent-client/index.ts",
+  output: {
+    name: "PercyAgent",
+    file: pkg.browser,
+    format: "umd"
+  },
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions: [".ts"] }),
+
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+
+    // Compile TypeScript/JavaScript files
+    babel({
+      extensions: [".ts"],
+      include: ["src/**"]
+    })
+  ]
+};
