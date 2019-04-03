3971e29a888528d5f4c4648e36f5a46b397585b8
Michael Padon <maprules1000@gmail.com>
2019-04-03 14:02:06 -0500

    Add stubbed static snapshot service

---
+++ src/services/static-snapshot-service.ts
@@ -0,0 +1,16 @@
+import logger from '../utils/logger'
+import {StaticSnapshotOptions} from './static-snapshot-options'
+
+export default class StaticSnapshotService {
+  constructor() {
+    // logger.info('calling constructor...')
+  }
+
+  async start(options: StaticSnapshotOptions) {
+    // logger.info('starting static snapshot service...')
+  }
+
+  async stop() {
+    // logger.info('stopping static snapshot service...')
+  }
+}
