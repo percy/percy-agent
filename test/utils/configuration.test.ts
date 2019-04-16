9b9d4606f35878e9f5c445515b4b8559f8f68da1
Michael Padon <maprules1000@gmail.com>
2019-04-16 17:23:24 -0500

    Update percy.yml configuration test

--- test/utils/configuration.test.ts
+++ test/utils/configuration.test.ts
@@ -8,11 +8,14 @@ describe('Configuration', () => {
     expect(subject.version).to.eql(1)
     expect(subject.snapshot.widths).to.eql([375, 1280])
     expect(subject.snapshot['min-height']).to.eql(1024)
+    expect(subject.static_site['base-url']).to.eql('/blog')
+    expect(subject.static_site['snapshot-files']).to.eql('\\.html$')
+    expect(subject.static_site['ignore-files']).to.eql('\\.htm$')
   })
 
   it('gracefully handles a missing file', () => {
     const subject = configuration('test/support/.file-does-not-exist.yml')
 
-    expect(subject).to.eql({version: 1.0, snapshot: {}})
+    expect(subject).to.eql({version: 1.0, snapshot: {}, static_site: {}})
   })
 })
