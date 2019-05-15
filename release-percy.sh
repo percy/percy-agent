af5ccc6ac3b6134113d84a6e4ab8a255e5bcb7d2
David Jones <dgjones@gmail.com>
2019-05-15 14:15:26 -0700

    add some comments to release script

--- release-percy.sh
+++ release-percy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-sed -i 's/@percy\/agent/percy/g' package.json
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+# This script is responsible for shipping the `percy` shadow package.
+# https://www.npmjs.com/package/percy
+#
+# Release of the `@percy/agent` package is handled with semantic-release
 
-# We ask NPM if it has this version already
+# First we jam `percy` into the package name
+sed -i 's/@percy\/agent/percy/g' package.json
+
+# Next we grab the local version and check to see if that version exists on npm
 packageAlreadyReleased=$(npm view percy versions | grep $(node -p "require('./package.json').version"))
 
+# If the package with that version has not yet been released, go ahead and release it.
 if [ !$packageAlreadyReleased ]; then
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
   npm publish
 else
   echo "Skipping percy package publishing because the desired version has already been published."
