834003c8bd9f7d3097b1273dfb7b98dc11dbb456
Robert DeLuca <robertdeluca19@gmail.com>
2019-05-23 16:49:49 -0500

    Setup Karma over Testem
    This will allow us to run our tests in an iframe (so it's isolated from any test
    output), have a warmed cache & watch server. Also cleans up some of the TODOs
    that were present in the old `index.html` file.

---
+++ test/percy-agent-client/index.js
@@ -0,0 +1,2 @@
+const requireTest = require.context('.', true, /.test/);
+requireTest.keys().forEach(requireTest);
---
+++ test/percy-agent-client/karma.conf.js
@@ -0,0 +1,45 @@
+module.exports = config => {
+  config.set({
+    frameworks: ["mocha"], // sets up the Mocha framework automatically
+    reporters: ["mocha"], // reports back to the CLI formatted like Mocha
+    browsers: ["Chrome"], // automatically launches chrome to run our tests
+
+    files: [
+      // karma-webpack will watch our files
+      { pattern: "./index.js", watched: false }
+    ],
+
+    preprocessors: {
+      // tells Karma that we'll be using Webpack to process this file
+      "./index.js": ["webpack"]
+    },
+
+    // Mocha reporter options
+    mochaReporter: {
+      showDiff: true
+    },
+
+    // our webpack config exports a function
+    webpack: {
+      mode: "development",
+      module: {
+        rules: [
+          {
+            test: /\.ts?$/,
+            use: "babel-loader",
+            exclude: /node_modules/
+          }
+        ]
+      },
+      resolve: {
+        extensions: [".ts", ".js"]
+      }
+    },
+
+    webpackMiddleware: {
+      stats: "minimal"
+    }
+    // for more Karma config options, check out the documentation
+    // http://karma-runner.github.io/2.0/config/configuration-file.html
+  });
+};
