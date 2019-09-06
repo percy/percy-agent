6899a83ed391fdef2d6ac94ab7db59883c0b0ccf
Wil Wilsman <wil@wilwilsman.com>
2019-09-06 12:17:27 -0500

    Update winston to 3.x
    The biggest change is how formatting is applied. Winston 3 does not have default
    formatting and format-specific options like showLevel and label have been moved
    into a more robust format syntax.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,34 +1,34 @@
 import * as colors from 'colors'
 import * as winston from 'winston'
 
-const logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      level: (process.env.LOG_LEVEL || 'info'),
-      showLevel: false,
-      label: colors.magenta('percy'),
-    }),
-  ],
+const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
+
+const consoleTransport = new winston.transports.Console({
+  level: LOG_LEVEL,
+  format: winston.format.combine(
+    winston.format.label({ label: colors.magenta('percy') }),
+    winston.format.printf(({ label, message }) => `[${label}] ${message}`),
+  ),
 })
 
-export function profile(
-  id: string, meta?: any,
-  callback?: (err: Error, level: string, msg: string, meta: any) => void,
-): winston.LoggerInstance | undefined {
-  if (process.env.LOG_LEVEL === 'debug') {
-    // Only pass the callback through if it is defined, because the winston.Logger implementation
-    // does not behave as expected if you pass a null callback (it will ignore the meta parameter).
-    if (callback) {
-      return logger.profile(id, id, meta, callback)
-    } else {
-      return logger.profile(id, id, meta)
-    }
+const logger = winston.createLogger({
+  transports: [consoleTransport],
+})
+
+export function profile(id: string, meta?: any): winston.Logger | undefined {
+  if (LOG_LEVEL === 'debug') {
+    return logger.profile(id, { level: 'debug', ...meta })
   }
 }
 
 export function logError(error: any) {
   logger.error(`${error.name} ${error.message}`)
   logger.debug(error)
+}
+
+export function createFileLogger(filename: string) {
+  const fileTransport = new winston.transports.File({ filename, level: 'debug' })
+  return winston.createLogger({ transports: [consoleTransport, fileTransport] })
 }
 
 export default logger
