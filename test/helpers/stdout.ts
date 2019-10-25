64a7bf6aa3fdc5e6a0e1189063ce50185e0ad704
Wil Wilsman <wil@wilwilsman.com>
2019-10-25 17:40:54 -0500

    :white_check_mark: Update helpers to not throw oclif exit errors

--- test/helpers/stdout.ts
+++ test/helpers/stdout.ts
@@ -2,16 +2,28 @@ import * as std from 'stdout-stderr'
 
 export async function captureStdOut(callback: any): Promise<string> {
   std.stdout.start()
-  await callback()
-  std.stdout.stop()
+
+  try {
+    await callback()
+  } catch (err) {
+    if (!err.oclif || err.oclif.exit !== 0) { throw err }
+  } finally {
+    std.stdout.stop()
+  }
 
   return std.stdout.output
 }
 
 export async function captureStdErr(callback: any): Promise<string> {
   std.stderr.start()
-  await callback()
-  std.stderr.stop()
+
+  try {
+    await callback()
+  } catch (err) {
+    if (!err.oclif || err.oclif.exit !== 0) { throw err }
+  } finally {
+    std.stderr.stop()
+  }
 
   return std.stderr.output
 }
