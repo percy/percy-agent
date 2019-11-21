f0e0b4524e8437d725ab12f99bb3894be1dad3c8
Robert DeLuca <robertdeluca19@gmail.com>
2019-11-21 11:44:15 -0600

    test: Add failing test for snapshots being added after finalize

---
+++ test/acceptance/asset-discovery.test.js
@@ -0,0 +1,26 @@
+import expect from 'expect'
+import { execSync } from 'child_process'
+import { when } from 'interactor.js'
+
+import {
+  launch,
+  run,
+  setupApiProxy,
+  setupDummyApp
+} from './helpers'
+
+describe('Asset discovery', () => {
+  let proxy = setupApiProxy()
+  let dummy = setupDummyApp()
+
+  it('does not create snapshots while stopping a build', async () => {
+    let [stdout] = await run('percy exec -- node ./test/acceptance/dummy/snapshot-error.js');
+
+    expect(stdout).toHaveEntries([
+      '[percy] percy has started.',
+      "[percy] snapshot taken: 'Home Page - 0'",
+      '[percy] done.',
+      '[percy] finalized build #4: <<build-url>>'
+    ])
+  })
+})
