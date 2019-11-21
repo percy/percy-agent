684089bfa9943deed244bee5c7fc6a46cc951f74
Robert DeLuca <robertdeluca19@gmail.com>
2019-11-21 17:27:33 -0600

    Improve tests

--- test/acceptance/asset-discovery.test.js
+++ test/acceptance/asset-discovery.test.js
@@ -12,9 +12,13 @@ describe('Asset discovery', () => {
   it('does not create snapshots while stopping a build', async () => {
     let [stdout] = await run('percy exec -- node ./test/acceptance/dummy/snapshot-error.js');
 
-    // TODO: Assert the "waiting for x" output & for no snapshot logs to occur *after*
+    let finalizeReqDate = proxy.requests['/builds/123/finalize'][0].requestDate;
+    let lastSnapshotReqDate = proxy.requests['/builds/123/snapshots'].pop().requestDate;
+
+    expect(finalizeReqDate > lastSnapshotReqDate).toBe(true)
     expect(stdout).toHaveEntries([
       '[percy] percy has started.',
+      /^\[percy\] waiting for \d+ snapshots to complete\.\.\.$/m,
       "[percy] snapshot taken: 'Home Page - 0'",
       '[percy] done.',
       '[percy] finalized build #4: <<build-url>>'
